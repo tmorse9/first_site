ccbf63c2de9264d8bc3ebba29725c8089c836181
Tom <tom.morse926@gmail.com>
2017-12-29 10:41:45 -0500

    adding routes

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+require('./app/routes')(app);
+// app.get('/', (req, res) => {
+//   res.send('Hello world\n');
+// });
 
 app.listen(PORT, HOST);
 console.log("Running on http://${HOST}:${PORT}");
