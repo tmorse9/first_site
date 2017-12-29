5378e8349a8b3e13a458f8130e15675f43135be9
Tom <tom.morse926@gmail.com>
2017-12-29 16:49:05 -0500

    continuing to fix

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,11 +1,11 @@
 angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'views/home.html',
+      templateUrl: 'public/views/home.html',
       controller: 'MainController'
     })
     .when('/aboutme', {
-      templateUrl: 'views/aboutme.html',
+      templateUrl: 'public/views/aboutme.html',
       controller: 'ContentController'
     });
 
